0889b258d6c7b18863c438f322ca1a032e939381
SergioCabral <git@sergiocabral.com>
2020-06-25 02:44:34 -0400

    Suporte a mais de uma url.

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,12 +22,14 @@ printf "Entrypoint for docker image: nginx\n";
 NGINX_ARGS="$* $NGINX_ARGS";
 
 NGINX_EXECUTABLE=$(which nginx || echo "");
+NGINX_USER="nginx";
 SUFFIX_TEMPLATE=".template";
 DIR_CONF="/etc/nginx";
 DIR_CONF_BACKUP="$DIR_CONF.original";
 DIR_CONF_DOCKER="$DIR_CONF.conf";
 DIR_CONF_D="$DIR_CONF/conf.d";
 DIR_CONF_D_TEMPLATES="$DIR_CONF.templates";
+DIR_CERTIFICATES="$DIR_CONF.certificates";
 DIR_SCRIPTS="${DIR_SCRIPTS:-/root}";
 LS="ls --color=auto -CFl";
 
@@ -52,17 +54,17 @@ then
 
     $NGINX_EXECUTABLE;
     sleep 1;
-    $NGINX_EXECUTABLE -s stop;
-    sleep 1;
 
     printf "Configuring directories.\n";
+
+    mkdir -p $DIR_CERTIFICATES;
 
     mkdir -p $DIR_CONF_BACKUP && cp -R $DIR_CONF/* $DIR_CONF_BACKUP;
     mkdir -p $DIR_CONF_DOCKER && cp -R $DIR_CONF/* $DIR_CONF_DOCKER;
     rm -R $DIR_CONF;
     ln -s $DIR_CONF_DOCKER $DIR_CONF;
 
-    mkdir -p $DIR_CONF_D_TEMPLATES;
+    mkdir -p $DIR_CONF_D_TEMPLATES;    
 
     if [ -d "$DIR_CONF_D_TEMPLATES" ] && [ ! -z "$(ls -A $DIR_CONF_D_TEMPLATES)" ];
     then
@@ -86,6 +88,9 @@ then
     $LS -d $DIR_CONF $DIR_CONF_BACKUP $DIR_CONF_DOCKER $DIR_CONF_D_TEMPLATES;
 else
     printf "This is NOT the first run.\n";
+
+    $NGINX_EXECUTABLE;
+    sleep 1;
 fi
 
 printf "Tip: Use files $DIR_CONF_D_TEMPLATES/*$SUFFIX_TEMPLATE to make the files in the $DIR_CONF_D directory with replacement of environment variables with their values.\n";
@@ -96,9 +101,11 @@ INDEX_HOST=1;
 while [ -n "$(VAR_NAME="HOST${INDEX_HOST}_URL"; echo "${!VAR_NAME}")" ];
 do
     VAR_NAME="HOST${INDEX_HOST}_URL";
-    URL=${!VAR_NAME};
+    VAR_NAME="HOST${INDEX_HOST}_URL";
+    URLS_ALL=${!VAR_NAME};
+    readarray -t URLS < <($DIR_SCRIPTS/split-to-lines.sh " " "$URLS_ALL");
 
-    printf "Configuring reverse proxy for HOST$INDEX_HOST ($URL).\n";
+    printf "Configuring reverse proxy for HOST$INDEX_HOST (${URLS[0]}).\n";
 
     VAR_NAME="HOST${INDEX_HOST}_SERVER";
     SERVER=${!VAR_NAME};
@@ -132,7 +139,17 @@ do
     then
         printf "\n";
         
-        printf "    Url:            $URL\n";
+        printf "    Total url:      ${#URLS[@]}\n";        
+
+        INDEX_URL=0;
+        for URL in ${URLS[@]};
+        do
+            INDEX_URL=$((INDEX_URL + 1));
+
+            PADDING=$( test $INDEX_URL -lt 10 && echo " ");
+            printf "    - Url $INDEX_URL:$PADDING       $URL\n";
+        done
+
         printf "    Server name:    $SERVER_NAME\n";
         printf "    Server port:    $SERVER_PORT\n";
         printf "    Let's Encrypt:  $SSL_ENABLE\n";
@@ -165,18 +182,46 @@ do
         fi
         printf "\n";
 
-        FILE_CONF="$DIR_CONF_D/$URL.conf";
+        FILE_CONF="$DIR_CONF_D/${URLS[0]}.conf";
         FILE_PASSWD="$FILE_CONF.htpasswd";
 
         rm -f $FILE_CONF;
         rm -f $FILE_PASSWD;
+
+        if [ "$SSL_ENABLE" = true ];
+        then
+            # $DIR_CERTIFICATES;
+            # $SSL_EMAIL
+
+            printf "Requesting certificate to Let's Encrypt.\n";
+
+            touch $FILE_CONF;
+            DIR_WWW="/tmp/letsencrypt";
+            mkdir -p $DIR_WWW;
+            chmod -R 777 $DIR_WWW;
+            
+            echo "server {" >> $FILE_CONF;
+            echo "    listen                       80;" >> $FILE_CONF;
+            echo "    listen                       [::]:80;" >> $FILE_CONF;
+            echo "    server_name                  $URLS_ALL;" >> $FILE_CONF;        
+            echo "    location / {" >> $FILE_CONF;
+            echo "        root                     $DIR_WWW;" >> $FILE_CONF;
+            echo "    }" >> $FILE_CONF;
+            echo "}" >> $FILE_CONF;
+
+            $NGINX_EXECUTABLE -s reload;
+            sleep 1;
+
+            #TODO rm -R $DIR_WWW;
+            rm -f $FILE_CONF;
+        fi
 
         if [ "$AUTH_ENABLE" = true ];
         then
             printf "Configuring access control.\n";
             
             touch $FILE_PASSWD;
-            chown root:nginx $FILE_PASSWD;
+            chown root:$NGINX_USER $FILE_PASSWD;
             chmod 640 $FILE_PASSWD;
 
             INDEX_AUTH=0;
@@ -195,14 +240,14 @@ do
         echo "server {" >> $FILE_CONF;
         echo "    listen                                 80;" >> $FILE_CONF;
         echo "    listen                                 [::]:80;" >> $FILE_CONF;
-        echo "    server_name                            $URL;" >> $FILE_CONF;        
+        echo "    server_name                            $URLS_ALL;" >> $FILE_CONF;
         echo "    location / {" >> $FILE_CONF;
         echo "        proxy_pass                         http://$SERVER_NAME:$SERVER_PORT;" >> $FILE_CONF;
         echo "" >> $FILE_CONF;
 
         if [ "$AUTH_ENABLE" = true ];
         then
-            echo "        auth_basic                         \"Enter your access credentials to enter $URL\";" >> $FILE_CONF;
+            echo "        auth_basic                         \"Enter your access credentials to enter ${URLS[0]}\";" >> $FILE_CONF;
             echo "        auth_basic_user_file               $FILE_PASSWD;" >> $FILE_CONF;
             echo "" >> $FILE_CONF;
         fi
@@ -236,4 +281,8 @@ fi
 
 printf "Starting nginx.\n";
 
+$NGINX_EXECUTABLE -s stop;
+sleep 1;
 $NGINX_EXECUTABLE -g "daemon off;" ${NGINX_ARGS};
+
+sleep infinity;
